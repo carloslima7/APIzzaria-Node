733be64928eaa8da4ecb4b54c266dd092ef01fa0
Carlos Alberto Lima <carlos.alberto@full-fit.local>
2021-10-20 17:25:32 -0300

    Schema Product Added

---
+++ model/products.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new Schema({
+    itemCode: {
+        type: String,
+        require: true,
+        unique: true,
+        uppercase: true
+    },
+    itemName: {
+        type: String,
+        require: true
+    },
+    type: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        default: 0.00
+    },
+    cost: {
+        type: Number,
+        default: 0.00
+    },
+    stock: {
+        type: Number,
+        default: 0.00
+    },
+    feedStock: {
+        feedCode: String,
+        feedName: String,
+        cost: {
+            type: Number,
+            default: 0.00
+        }
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    },
+    createUser: {
+        type: String,
+        require: true
+    },
+    updateAt: {
+        type: Date,
+        default: Date.now
+    },
+    updateUser: {
+        type: String,
+        require: true
+    }
+}
+)
+
+const Product = mongoose.model('Product', ProductSchema)
+
+module.exports = Product;
